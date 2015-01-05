a66dd91a0f1a781b1bc6b787b2e948b5b8a5210a
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-01-04 17:41:30 -0700

    Add Verse Data Through Alma

--- src/scriptures/bom/data.js
+++ src/scriptures/bom/data.js
@@ -2,12 +2,64 @@ var data = {
   name: 'Book of Mormon',
   books: [
     {
-      name: '1 Ne',
-      chapters: [ 10, 10, 10 ]
+      name: '1 Nephi',
+      chapters: [ 20, 24, 31, 38, 22, 6, 22, 38, 6, 22, 36, 23, 42, 30, 36, 39, 55, 25, 24, 22, 26, 31 ]
     },
     {
-      name: '2 Ne',
-      chapters: [ 10, 10, 10 ]
+      name: '2 Nephi',
+      chapters: [ 32, 30, 25, 35, 34, 18, 11, 25, 54, 25, 8, 22, 26, 6, 30, 13, 25, 22, 21, 34, 16, 6, 22, 32, 30, 33, 35, 32, 14, 18, 21, 9, 15 ]
+    },
+    {
+      name: 'Jacob',
+      chapters: [ 19, 35, 14, 18, 77, 13, 27 ]
+    },
+    {
+      name: 'Enos',
+      chapters: [ 27 ]
+    },
+    {
+      name: 'Jarom',
+      chapters: [ 15 ]
+    },
+    {
+      name: 'Omni',
+      chapters: [ 30 ]
+    },
+    {
+      name: 'Words of Mormon',
+      chapters: [ 18 ]
+    },
+    {
+      name: 'Mosiah',
+      chapters: [ 18, 41, 27, 30, 15, 7, 33, 21, 19, 22, 29, 37, 35, 12, 31, 15, 20, 35, 29, 26, 36, 16, 39, 25, 24, 39, 37, 20, 47 ]
+    },
+    {
+      name: 'Alma',
+      chapters: [ 33, 38, 27, 20, 62, 8, 27, 32, 34, 32, 46, 37, 31, 29, 19, 21, 39, 43, 36, 30, 23, 35, 18, 30, 17, 37, 30, 14, 17, 60, 38, 43, 23, 41, 16, 30, 47, 15, 19, 26, 15, 31, 54, 24, 24, 41, 36, 25, 30, 40, 37, 40, 23, 24, 35, 57, 36, 41, 13, 36, 21, 52, 17 ]
+    },
+    {
+      name: 'Helaman',
+      chapters: [  ]
+    },
+    {
+      name: '3 Nephi',
+      chapters: [  ]
+    },
+    {
+      name: '4 Nephi',
+      chapters: [  ]
+    },
+    {
+      name: 'Mormon',
+      chapters: [  ]
+    },
+    {
+      name: 'Ether',
+      chapters: [  ]
+    },
+    {
+      name: 'Moroni',
+      chapters: [  ]
     }
   ]
 };
